9f986a16974b725b777335a188877ac185fc8c28
Dave Tucker <dave@dtucker.co.uk>
2022-09-08 19:32:54 +0000

    aya: Add PinnedProgram
    This commit adds PinnedProgram which allows the creation of a Program
    from a path on bpffs. This is useful to be able to call `attach` or
    other APIs for programs that are already loaded to the kernel.
    Unfortunately figuring out the concrete type of program is hard given
    the information in the kernel so this may not work for all program
    types.
    
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- aya/src/bpf.rs
+++ aya/src/bpf.rs
@@ -574,7 +574,7 @@ impl<'a> BpfLoader<'a> {
                         ProgramSection::CgroupSock { attach_type, .. } => {
                             Program::CgroupSock(CgroupSock {
                                 data: ProgramData::new(prog_name, obj, btf_fd, verifier_log_level),
-                                attach_type: *attach_type,
+                                attach_type: Some(*attach_type),
                             })
                         }
                         ProgramSection::CgroupDevice { .. } => {
