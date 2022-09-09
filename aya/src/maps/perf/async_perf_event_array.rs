123f553767d8882ee9c3c4131c278277f578a3a2
Andrew Stoycos <astoycos@redhat.com>
2022-09-09 16:12:28 -0400

    Add try_from map ref for AsyncPerfEventArray
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- aya/src/maps/perf/async_perf_event_array.rs
+++ aya/src/maps/perf/async_perf_event_array.rs
@@ -12,7 +12,7 @@ use tokio::io::unix::AsyncFd;
 
 use crate::maps::{
     perf::{Events, PerfBufferError, PerfEventArray, PerfEventArrayBuffer},
-    Map, MapError, MapRefMut,
+    Map, MapError, MapRefMut, MapRef,
 };
 
 /// A `Future` based map that can be used to receive events from eBPF programs using the linux
@@ -200,6 +200,14 @@ impl TryFrom<MapRefMut> for AsyncPerfEventArray<MapRefMut> {
     type Error = MapError;
 
     fn try_from(a: MapRefMut) -> Result<AsyncPerfEventArray<MapRefMut>, MapError> {
+        AsyncPerfEventArray::new(a)
+    }
+}
+
+impl<'a> TryFrom<&'a mut Map> for AsyncPerfEventArray<&'a mut Map> {
+    type Error = MapError;
+
+    fn try_from(a: &'a mut Map) -> Result<AsyncPerfEventArray<&'a mut Map>, MapError> {
         AsyncPerfEventArray::new(a)
     }
 }
