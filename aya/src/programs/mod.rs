04bb46a40239785dd8d51d2a07395d171554dd2b
Andrew Stoycos <astoycos@redhat.com>
2023-07-26 14:40:44 -0400

    remove programfd in favor of ownedfd
    This commit removes the ProgramFd wrapper of RawFd and deduplicates some
    code. Specifically it adds a new constructor for ProgramInfo which
    allows it to be generated from an OwnedFd.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- aya/src/programs/mod.rs
+++ aya/src/programs/mod.rs
@@ -69,7 +69,7 @@ use libc::ENOSPC;
 use std::{
     ffi::CString,
     io,
-    os::fd::{AsFd, AsRawFd, OwnedFd, RawFd},
+    os::fd::{AsFd, AsRawFd, FromRawFd as _, OwnedFd, RawFd},
     path::{Path, PathBuf},
     time::{Duration, SystemTime},
 };
@@ -214,16 +214,6 @@ pub enum ProgramError {
     /// An error occurred while working with IO.
     #[error(transparent)]
     IOError(#[from] io::Error),
-}
-
-/// A [`Program`] file descriptor.
-#[derive(Copy, Clone)]
-pub struct ProgramFd(RawFd);
-
-impl AsRawFd for ProgramFd {
-    fn as_raw_fd(&self) -> RawFd {
-        self.0
-    }
 }
 
 /// eBPF program type.
@@ -375,7 +365,7 @@ impl Program {
     ///
     /// Can be used to add a program to a [`crate::maps::ProgramArray`] or attach an [`Extension`] program.
     /// Can be converted to [`RawFd`] using [`AsRawFd`].
-    pub fn fd(&self) -> Option<ProgramFd> {
+    pub fn fd(&self) -> Option<OwnedFd> {
         match self {
             Program::KProbe(p) => p.fd(),
             Program::UProbe(p) => p.fd(),
@@ -736,8 +726,8 @@ macro_rules! impl_fd {
         $(
             impl $struct_name {
                 /// Returns the file descriptor of this Program.
-                pub fn fd(&self) -> Option<ProgramFd> {
-                    self.data.fd.map(|fd| ProgramFd(fd))
+                pub fn fd(&self) -> Option<OwnedFd> {
+                    self.data.fd.map(|fd| unsafe{ OwnedFd::from_raw_fd(fd) })
                 }
             }
         )+
@@ -931,13 +921,7 @@ macro_rules! impl_program_info {
                 /// Returns the file descriptor of this Program.
                 pub fn program_info(&self) -> Result<ProgramInfo, ProgramError> {
                     let fd = self.fd().ok_or(ProgramError::NotLoaded)?;
-
-                    bpf_prog_get_info_by_fd(fd.as_raw_fd(), &[])
-                        .map_err(|io_error| ProgramError::SyscallError {
-                            call: "bpf_prog_get_info_by_fd",
-                            io_error,
-                        })
-                        .map(ProgramInfo)
+                    ProgramInfo::new_from_fd(fd)
                 }
             }
         )+
@@ -976,6 +960,15 @@ impl_program_info!(
 pub struct ProgramInfo(bpf_prog_info);
 
 impl ProgramInfo {
+    fn new_from_fd(fd: OwnedFd) -> Result<Self, ProgramError> {
+        bpf_prog_get_info_by_fd(fd.as_raw_fd(), &[])
+            .map_err(|io_error| ProgramError::SyscallError {
+                call: "bpf_prog_get_info_by_fd",
+                io_error,
+            })
+            .map(ProgramInfo)
+    }
+
     /// The name of the program as was provided when it was load. This is limited to 16 bytes
     pub fn name(&self) -> &[u8] {
         let length = self
@@ -1124,14 +1117,7 @@ impl Iterator for ProgramsIter {
                             call: "bpf_prog_get_fd_by_id",
                             io_error,
                         })
-                        .and_then(|fd| {
-                            bpf_prog_get_info_by_fd(fd.as_raw_fd(), &[])
-                                .map_err(|io_error| ProgramError::SyscallError {
-                                    call: "bpf_prog_get_info_by_fd",
-                                    io_error,
-                                })
-                                .map(ProgramInfo)
-                        }),
+                        .and_then(ProgramInfo::new_from_fd),
                 )
             }
             Ok(None) => None,
